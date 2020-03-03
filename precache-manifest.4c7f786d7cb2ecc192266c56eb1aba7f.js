5e1bef63fd9f7871fc1f33b40260933d67c28486
Pretzky <prestonj.russell@gmail.com>
2020-03-02 19:41:53 -0700

    Updates

---
+++ precache-manifest.4c7f786d7cb2ecc192266c56eb1aba7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07f4540cd6e1f57fa142c5f5690f5ab",
+    "url": "/comic-organizer/index.html"
+  },
+  {
+    "revision": "f137e5e5208d638d586b",
+    "url": "/comic-organizer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f05b0f0f2ad4064f0023",
+    "url": "/comic-organizer/static/js/2.0acfa56a.chunk.js"
+  },
+  {
+    "revision": "7a37ef64567e4fbb9221b2a105cd1cf2",
+    "url": "/comic-organizer/static/js/2.0acfa56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f137e5e5208d638d586b",
+    "url": "/comic-organizer/static/js/main.90836f9a.chunk.js"
+  },
+  {
+    "revision": "e03dc00cc744fc30ba21",
+    "url": "/comic-organizer/static/js/runtime-main.a082acb3.js"
+  }
+]);
